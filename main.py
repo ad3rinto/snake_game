4cda870c4e4f4b8b513c74eb387856c733e0185b
ad3rinto <aad3rinto@gmail.com>
2023-10-31 15:34:32 +0000

    enable change direction with direction key and onkey event listener

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from turtle import Screen
 from snake import Snake
 
 screen = Screen()
+screen.listen()
+
+
 screen.setup(width=900, height=700)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_on = True
 
