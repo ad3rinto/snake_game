2c8a11f48656d54add2e446e700ee9e81be5dfb5
ad3rinto <aad3rinto@gmail.com>
2023-11-02 13:24:34 +0000

    build screen refresh, and food component

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.listen()
 
-
-screen.setup(width=900, height=700)
+screen.setup(width=900, height=900)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -23,4 +24,10 @@ while game_on:
     screen.update()
     time.sleep(.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+
+
 screen.exitonclick()
