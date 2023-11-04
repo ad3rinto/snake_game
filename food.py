660161c454f4831cbdf685f57696a86ca28ecbc3
aaderinto <aad3rinto@gmail.com>
2023-11-04 22:35:13 +0000

    changed food attribute shape and colour

--- food.py
+++ food.py
@@ -5,10 +5,10 @@ import random
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color('blue')
+        self.shapesize(stretch_len=0.7, stretch_wid=0.7)
+        self.color('red')
         self.speed('fastest')
         random_x = random.randint(-400, 400)
         random_y = random.randint(-400, 400)
